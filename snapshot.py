599738878d7b2cdf103d7201d7dbfc6e9e9a2f59
hendos43 <55980174+hendos43@users.noreply.github.com>
2020-03-09 17:15:28 +0000

    comments added

--- snapshot.py
+++ snapshot.py
@@ -4,22 +4,30 @@ import matplotlib
 import gpiozero
 from PIL import Image
 
+# create new blank image for compositing edge detection images onto
 im = Image.new("RGB", (640, 480), "white")
 im.save('result.png')
 
+# open connection to webcam
 camera = cv2.VideoCapture(0)
+
+# on button press, collect images
+# eventually, until button is pressed again
+
 for i in range(10):
     return_value, image = camera.read()
     edges = cv2.Canny(image, 100, 200)
     edges = cv2.bitwise_not(edges)
     cv2.imwrite('opencv'+str(i)+'.png', edges)
     
+# on button press (eventually when button is pressed again)
+# multiply all images together and export as one image
+
 image = cv2.imread('opencv1.png')    
 result = cv2.imread('result.png')
 combined = cv2.multiply(image, result)
 cv2.imwrite('result.png', combined)
     
-
-    
+# on button press, capture colour image of orange peel
     
 del(camera)
