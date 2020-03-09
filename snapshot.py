417bfaf1ec3ca175b29ba2c3d6eb469e6bee43ed
hendos43 <55980174+hendos43@users.noreply.github.com>
2020-03-09 17:26:45 +0000

    added write to specific directory

--- snapshot.py
+++ snapshot.py
@@ -4,6 +4,10 @@ import matplotlib
 import gpiozero
 from PIL import Image
 
+import os
+dirname = "test"
+os.mkdir(dirname)
+
 # create new blank image for compositing edge detection images onto
 im = Image.new("RGB", (640, 480), "white")
 im.save('result.png')
@@ -18,7 +22,7 @@ for i in range(10):
     return_value, image = camera.read()
     edges = cv2.Canny(image, 100, 200)
     edges = cv2.bitwise_not(edges)
-    cv2.imwrite('opencv'+str(i)+'.png', edges)
+    cv2.imwrite(os.path.join(dirname,'opencv'+str(i)+'.png'), edges)
     
 # on button press (eventually when button is pressed again)
 # multiply all images together and export as one image
